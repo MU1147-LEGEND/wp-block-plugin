4903ba206da1197606be66e94dc57ad18417e78d
Mohammad Ullah <ullahmohammad314@gmail.com>
2025-12-29 16:57:59 +0600

    fix: removed non working gradient on example.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1d45db5b4dd6fe03ce87');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '362b1c147032552c87a2');
